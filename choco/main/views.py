4a457585cde4b10e1f490496d9a75d978a33ea5e
Victoria <sadovskaya.vicka@yandex.ru>
2024-04-11 19:10:31 +0300

    Update (choco/main/views.py) and (choco/templates/main/main.htm)

--- choco/main/views.py
+++ choco/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 
+user_active = 'Гость'
+
+
 def main(request):
-    return render(request, 'main/main.html')
+    return render(request, 'main/blocks.html', context={'user_active': user_active})
