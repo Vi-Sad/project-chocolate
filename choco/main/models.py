12fb32ba14a84e5ebe78d3a5dfd210c6a2edecb2
Victoria <sadovskaya.vicka@yandex.ru>
2024-04-25 17:04:28 +0300

    Update model 'Products'. Displaying content from the database 'Products' to the template

--- choco/main/models.py
+++ choco/main/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     objects = None
     category = models.CharField(max_length=20, default=None)
     product_name = models.CharField(max_length=50, default=None)
+    description = models.CharField(max_length=150, default=None)
     price = models.IntegerField(default=0)
     grams = models.IntegerField(default=0)
 
