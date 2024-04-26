f35754404d06f90df4dedc44e144efef332cb8d2
Victoria <sadovskaya.vicka@yandex.ru>
2024-04-26 20:53:06 +0300

    Creating a separate template for categories

--- choco/static/js/main.js
+++ choco/static/js/main.js
@@ -2,6 +2,7 @@ let input_category_festive = document.getElementById("festive");
 let input_category_standard = document.getElementById("standard");
 let input_category_animals = document.getElementById("animals");
 let input_category_items = document.getElementById("items");
+let input_category_all = document.getElementById("all");
 
 let div_category_festive = document.querySelector(".category_festive");
 let div_category_standard = document.querySelector(".category_standard");
@@ -11,6 +12,7 @@ function active_category_festive() {
   input_category_standard.disabled = false;
   input_category_animals.disabled = false;
   input_category_items.disabled = false;
+  input_category_all.disabled = false;
 
   div_category_festive.style.display = "block";
   div_category_standard.style.display = "none";
@@ -21,6 +23,7 @@ function active_category_standard() {
   input_category_standard.disabled = true;
   input_category_animals.disabled = false;
   input_category_items.disabled = false;
+  input_category_all.disabled = false;
 
   div_category_festive.style.display = "none";
   div_category_standard.style.display = "block";
@@ -31,6 +34,7 @@ function active_category_animals() {
   input_category_standard.disabled = false;
   input_category_animals.disabled = true;
   input_category_items.disabled = false;
+  input_category_all.disabled = false;
 
   div_category_festive.style.display = "none";
   div_category_standard.style.display = "none";
@@ -41,12 +45,25 @@ function active_category_items() {
   input_category_standard.disabled = false;
   input_category_animals.disabled = false;
   input_category_items.disabled = true;
+  input_category_all.disabled = false;
 
   div_category_festive.style.display = "none";
   div_category_standard.style.display = "none";
+}
+
+function active_category_all() {
+  input_category_festive.disabled = false;
+  input_category_standard.disabled = false;
+  input_category_animals.disabled = false;
+  input_category_items.disabled = false;
+  input_category_all.disabled = true;
+
+  div_category_festive.style.display = "block";
+  div_category_standard.style.display = "block";
 }
 
 input_category_festive.onclick = active_category_festive;
 input_category_standard.onclick = active_category_standard;
 input_category_animals.onclick = active_category_animals;
 input_category_items.onclick = active_category_items;
+input_category_all.onclick = active_category_all;
