a85e5fce74d485ca80568d01a0dd8a687a51c160
Victoria <sadovskaya.vicka@yandex.ru>
2024-05-27 15:24:30 +0300

    Displaying products by category (unfinished)

--- choco/static/js/main.js
+++ choco/static/js/main.js
@@ -4,7 +4,6 @@ let input_category_festive = document.getElementById("festive");
 let input_category_standard = document.getElementById("standard");
 let input_category_animals = document.getElementById("animals");
 let input_category_items = document.getElementById("items");
-let input_category_all = document.getElementById("all");
 
 let div_category_festive = document.querySelector(".category_festive");
 let div_category_standard = document.querySelector(".category_standard");
@@ -16,7 +15,6 @@ function active_category_festive() {
   input_category_standard.disabled = false;
   input_category_animals.disabled = false;
   input_category_items.disabled = false;
-  input_category_all.disabled = false;
 
   div_category_festive.style.display = "block";
   div_category_standard.style.display = "none";
@@ -27,7 +25,6 @@ function active_category_festive() {
   input_category_standard.style.color = "rgb(55, 55, 55)";
   input_category_animals.style.color = "rgb(55, 55, 55)";
   input_category_items.style.color = "rgb(55, 55, 55)";
-  input_category_all.style.color = "rgb(55, 55, 55)";
 }
 
 function active_category_standard() {
@@ -35,7 +32,6 @@ function active_category_standard() {
   input_category_standard.disabled = true;
   input_category_animals.disabled = false;
   input_category_items.disabled = false;
-  input_category_all.disabled = false;
 
   div_category_festive.style.display = "none";
   div_category_standard.style.display = "block";
@@ -46,7 +42,6 @@ function active_category_standard() {
   input_category_standard.style.color = "grey";
   input_category_animals.style.color = "rgb(55, 55, 55)";
   input_category_items.style.color = "rgb(55, 55, 55)";
-  input_category_all.style.color = "rgb(55, 55, 55)";
 }
 
 function active_category_animals() {
@@ -54,7 +49,6 @@ function active_category_animals() {
   input_category_standard.disabled = false;
   input_category_animals.disabled = true;
   input_category_items.disabled = false;
-  input_category_all.disabled = false;
 
   div_category_festive.style.display = "none";
   div_category_standard.style.display = "none";
@@ -65,7 +59,6 @@ function active_category_animals() {
   input_category_standard.style.color = "rgb(55, 55, 55)";
   input_category_animals.style.color = "grey";
   input_category_items.style.color = "rgb(55, 55, 55)";
-  input_category_all.style.color = "rgb(55, 55, 55)";
 }
 
 function active_category_items() {
@@ -73,7 +66,6 @@ function active_category_items() {
   input_category_standard.disabled = false;
   input_category_animals.disabled = false;
   input_category_items.disabled = true;
-  input_category_all.disabled = false;
 
   div_category_festive.style.display = "none";
   div_category_standard.style.display = "none";
@@ -84,33 +76,13 @@ function active_category_items() {
   input_category_standard.style.color = "rgb(55, 55, 55)";
   input_category_animals.style.color = "rgb(55, 55, 55)";
   input_category_items.style.color = "grey";
-  input_category_all.style.color = "rgb(55, 55, 55)";
-}
-
-function active_category_all() {
-  input_category_festive.disabled = false;
-  input_category_standard.disabled = false;
-  input_category_animals.disabled = false;
-  input_category_items.disabled = false;
-  input_category_all.disabled = true;
-
-  div_category_festive.style.display = "block";
-  div_category_standard.style.display = "block";
-  div_category_animals.style.display = "block";
-  div_category_items.style.display = "block";
-
-  input_category_festive.style.color = "rgb(55, 55, 55)";
-  input_category_standard.style.color = "rgb(55, 55, 55)";
-  input_category_animals.style.color = "rgb(55, 55, 55)";
-  input_category_items.style.color = "rgb(55, 55, 55)";
-  input_category_all.style.color = "grey";
 }
 
 input_category_festive.onclick = active_category_festive;
 input_category_standard.onclick = active_category_standard;
 input_category_animals.onclick = active_category_animals;
 input_category_items.onclick = active_category_items;
-input_category_all.onclick = active_category_all;
+
 
 // Viewing chocolate additives (header)
 
