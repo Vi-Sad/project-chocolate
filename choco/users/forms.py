79e6cce74c5a125701274802f461873fd6e4b1fc
Victoria <sadovskaya.vicka@yandex.ru>
2024-08-10 11:14:33 +0300

    CSS styles have been applied to templates

--- choco/users/forms.py
+++ choco/users/forms.py
@@ -23,12 +23,9 @@ def is_valid_email(email):
 
 
 def is_valid_phone(phone):
-    if len(phone) >= 12:
-        if phone[0] == '8' or phone[0] == '+':
-            if phone[1] == '9' or phone[1] == '7':
-                return True
-            else:
-                return False
+    if len(phone) >= 11:
+        if phone[0] == '8' or phone[0] == '+' or phone[0] == '7':
+            return True
         else:
             return False
     else:
